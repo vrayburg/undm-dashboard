3965aec6f775600d2960a89e51b0b61cf0620f76
vrayburg <112010441+vrayburg@users.noreply.github.com>
2023-02-22 21:36:42 -0600

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -29,7 +29,7 @@ class Dashboard extends React.Component{
 
     poll = () => {
         this.setState({pollingCount: this.state.pollingCount + 1, oldDonations: this.state.donations});
-        fetch('https://events.dancemarathon.com/api/events/4589/donations?limit=5')
+        fetch('https://events.dancemarathon.com/api/events/5144/donations?limit=5')
             .then(response => response.json())
             .then(data => {
                 this.setState({donations: data})
@@ -61,10 +61,10 @@ class Dashboard extends React.Component{
         </Row>
         <Row style={{display: "inline-block"}}>
             <div className="countdown">
-                <span>🚀</span>
-                <Countdown date={new Date("Feb 26, 2022 23:45:00")} daysInHours={true}/>
-                <span style={{padding: 0}}> until Huskerthon blastoff!</span>
-                <span>🚀</span>
+                
+                <Countdown date={new Date("March 4, 2023 20:00:00")} daysInHours={true}/>
+                <span style={{padding: 0}}> until OU Dance Marathon Reveal!</span>
+                
             </div>
         </Row>
         {/* <Row>
